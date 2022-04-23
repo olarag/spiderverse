62a4874c4dc8a355d4b1be24d18d6062940ef935
olarag <oscar@difusion.mx>
2022-04-23 15:10:30 -0500

    se actualizó el contenido para probar clase

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,14 @@
-describe("Test Suite Dummy Description", () => {
-	test('Case 1 Dummy', () => {
-		const resultOfSomething = 1 + 2
-		expect(resultOfSomething).toBe(3);
+describe("Unit Test for Spiderman class", () => {
+	test('1) Create an spiderman object', () => {
+	
+		// Aquí escribimos el código que queremos usar tal cuál
+		// Quiero poder instanciar un objeto Spiderman con esta información
+		const andrewGarfield  = new Spiderman("Spiderman Sony", 31, "Andrew Garfiel", 2 "Sony")
+		// Validamos que este código funcione de la forma esperada
+		expect(andrewGarfield.name).toBe("Spiderman Sony")
+		expect(andrewGarfield.age).toBe(31)
+		expect(andrewGarfield.actor).toBe("Andrew Garfield")
+		expect(andrewGarfield.movies).toBe(31)
+		expect(andrewGarfield.studio).toBe("Sony")
 	});
 })
