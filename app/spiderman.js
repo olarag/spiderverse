e75c6599cacec26b50f9a29a1fea867a08e8eebb
olarag <oscar@difusion.mx>
2022-04-23 15:37:09 -0500

    se agregó la clase spiderman con sus atributos

--- app/spiderman.js
+++ app/spiderman.js
@@ -1,3 +1,11 @@
-class Spiderman {}
+class Spiderman {
+	constructor(name, age, actor, movies, studio){
+		this.name = name
+		this.age = age
+		this.actor = actor
+		this.movies = movies
+		this.studio = studio
+	}
+}
 
 module.exports = Spiderman
