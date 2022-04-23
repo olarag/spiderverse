6b0dc388cc3575d7acfd248d3624f317d3c7ee52
olarag <oscar@difusion.mx>
2022-04-23 15:37:41 -0500

    se actualizó contenido de test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -5,12 +5,12 @@ describe("Unit Test for Spiderman class", () => {
 	
 		// Aquí escribimos el código que queremos usar tal cuál
 		// Quiero poder instanciar un objeto Spiderman con esta información
-		const andrewGarfield  = new Spiderman("Spiderman Sony", 31, "Andrew Garfiel", 2 "Sony")
+		const andrewGarfield  = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
 		// Validamos que este código funcione de la forma esperada
 		expect(andrewGarfield.name).toBe("Spiderman Sony")
 		expect(andrewGarfield.age).toBe(31)
 		expect(andrewGarfield.actor).toBe("Andrew Garfield")
-		expect(andrewGarfield.movies).toBe(31)
+		expect(andrewGarfield.movies).toBe(2)
 		expect(andrewGarfield.studio).toBe("Sony")
 	});
 })
